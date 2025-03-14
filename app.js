8216b3115340b7746e5965767fafe2b617153da9
Rafael Cordeiro <rafaelsilvacordeiro12@gmail.com>
2025-03-14 15:22:27 -0400

    A ultima atualização de app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Declara uma variável do tipo array para armazenar os nomes dos amigos
 let amigos = [];
 
